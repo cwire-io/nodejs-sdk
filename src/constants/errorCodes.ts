ce0e81c38eb50be1d14c2aa68be6abcc7bca0996
leon <leonlowitzki@gmail.com>
2020-09-22 21:56:37 +0200

    typo

--- src/constants/errorCodes.ts
+++ src/constants/errorCodes.ts
@@ -3,7 +3,7 @@ export const errorCodes: { [key: number]: string } = {
   400: "Bad Request",
   401: "Unauthorized",
   404: "Page not found :(",
-  422: "Wrong body entitys",
+  422: "Wrong body entities",
   500: "Internal server error",
   503: "Service Unavailable",
 };
